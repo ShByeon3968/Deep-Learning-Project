5a14fcf8ef723db5958f9ad7fd52309bed6b6942
SangHyun Byun <101382267+ShByeon3968@users.noreply.github.com>
2025-04-14 00:23:45 +0900

    transformer

--- Transformer/model.py
+++ Transformer/model.py
@@ -1,10 +1,40 @@
-import math
-from typing import Optional, List
 import torch
 import torch.nn as nn
-from labml import tracker
+import torch.nn.functional as F
 
-class PrepareForMultiHeadAttention(nn.Module):
-    def __init__(self, d_model:int, heads:int, d_k:int,bias:bool):
+class SelfAttention(nn.Module):
+    def __init__(self,embed_dim,atten_dim):
         super().__init__()
-        self.linear = nn.Linear(d_model, heads * d_k, bias=bias)
+        self.query = nn.Linear(embed_dim,atten_dim,bias=False)
+        self.key = nn.Linear(embed_dim,atten_dim,bias=False)
+        self.value = nn.Linear(embed_dim,atten_dim,bias=False)
+
+    def forward(self,x):
+        query = self.query(x)
+        key = self.key(x)
+        value = self.value(x)
+
+        scores = torch.matmul(query,key.transpose(-2,-1))
+        scores = scores / key.size(-1)**0.5
+
+        attention_weight = F.softmax(scores, dim=-1)
+        weighted_values = torch.matmul(attention_weight,value)
+        return weighted_values
+    
+class MultiheadAttention(nn.Module):
+    def __init__(self,embed_dim, num_heads):
+        super().__init__()
+        attention_dim = embed_dim // num_heads
+        self.attentions = nn.ModuleList([SelfAttention(embed_dim,attention_dim) for _ in range(num_heads)])
+        self.fc = nn.Linear(embed_dim,embed_dim)
+
+    def forward(self,x):
+        head_outputs = []
+        for attention in self.attentions:
+            head_output = attention(x)
+            head_outputs.append(head_output)
+        concatenated_heads = torch.cat(head_outputs, dim=-1)
+        print("concatenated_heads", concatenated_heads.shape)
+        output = self.fc(concatenated_heads)
+        print("output", output.shape)
+        return output
